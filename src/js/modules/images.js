f4d9e49660fe3939256b514500499a25eb3b0696
Nicolas <nicolaslinaresoli@gmail.com>
2026-08-05 17:41:15 -0300

    adicionado animação de onLoad

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -6,12 +6,23 @@ export default function initImages(){
 const btnNext = document.querySelector('#next');
 
 function getImage(){
+    const catImage = document.querySelector('#catImage');
+
+    catImage.classList.add("loading");
+
     fetch('https://cataas.com/cat')
     .then(response => response.blob())
-    .then(body => {
-        const blobUrl = URL.createObjectURL(body)
-        const catDiv = document.querySelector('#catImage');
-        catDiv.src = blobUrl
+    .then(blob => {
+        const blobUrl = URL.createObjectURL(blob)
+
+        const img = new Image();
+
+        img.onload = () => {
+            catImage.src = blobUrl;
+            catImage.classList.remove("loading")
+        };
+
+        img.src = blobUrl;
     })
 }
 
